05abd8291c5132818a3f97fbd5b109037b484638
Juan D Ortiz C <soydavex@gmail.com>
2020-10-08 19:23:04 -0500

    Ejercicio Tres Realizado

---
+++ ejercicioTres.py
@@ -0,0 +1,9 @@
+numeroUno = int (19)
+numeroDos = int (10)
+
+if (numeroUno > numeroDos):
+    print ("numeroUno es Mayor")
+elif (numeroDos > numeroUno):
+    print ("numeroDos es Mayor")
+else:
+    print("Son iguales")
