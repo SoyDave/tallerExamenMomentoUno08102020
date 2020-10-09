dd98dce551b0e1b7f0890719b8a13e397f3f76d3
Juan D Ortiz C <soydavex@gmail.com>
2020-10-08 19:14:35 -0500

    Ejercicio Dos Realizado

---
+++ ejercicioDos.py
@@ -0,0 +1,9 @@
+numeroUno = 50
+numeroDos = 50
+
+suma = numeroUno + numeroDos
+resta = numeroUno - numeroDos
+producto = numeroUno * numeroDos
+division = numeroUno / numeroDos
+
+print (suma, resta, producto, division)
