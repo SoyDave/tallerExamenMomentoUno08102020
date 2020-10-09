ac4cf7bb4af56bcdd4414ee2926740b360920113
Juan D Ortiz C <soydavex@gmail.com>
2020-10-08 19:30:47 -0500

    Ejercicio Cuatro Realizado

---
+++ ejercicioCuatro.py
@@ -0,0 +1,10 @@
+numeroUno = 3
+numeroDos = 6
+numeroTres = 9
+
+if (numeroUno > numeroDos and numeroUno > numeroTres):
+    print ("numeroUno es Mayor")
+elif (numeroDos > numeroUno and numeroDos > numeroTres):
+    print ("numeroDos es Mayor")
+else:
+    print ("numeroTres es Mayor")
