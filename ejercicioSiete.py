9347bbf961bc0989217c81fc727a04dd3fcc1eb9
Juan D Ortiz C <soydavex@gmail.com>
2020-10-08 19:56:18 -0500

    Ejercicio Siete Realizado

---
+++ ejercicioSiete.py
@@ -0,0 +1,9 @@
+chicos = 19
+chicas = 21
+
+totalEstudiantes = chicos + chicas
+
+porcentajeChicos = (chicos * 100) / totalEstudiantes
+porcentajeChicas = (chicas * 100) / totalEstudiantes
+
+print (porcentajeChicos, porcentajeChicas)
