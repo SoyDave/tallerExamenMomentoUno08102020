44afe8c4a29b8b6712c804aa404f042abbe39279
Juan D Ortiz C <soydavex@gmail.com>
2020-10-08 19:51:00 -0500

    Ejercicio Seis Realizado

---
+++ ejercicioSeis.py
@@ -0,0 +1,10 @@
+import math
+
+numeroUno = input()
+
+if(numeroUno <= 0):
+    print("el numero no debe ser menor a cero")
+elif(numeroUno > 0):
+    raiz = math.sqrt(numeroUno)
+    cuadrado = math.pow(numeroUno, 2)
+    print(raiz,cuadrado)
