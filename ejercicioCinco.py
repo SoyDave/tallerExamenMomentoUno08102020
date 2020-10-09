f07f3c969772b0582269e59d5b4644012b1e2083
Juan D Ortiz C <soydavex@gmail.com>
2020-10-08 19:37:09 -0500

    Ejercicio Cinco Realizado

---
+++ ejercicioCinco.py
@@ -0,0 +1,10 @@
+numeroUno = input()
+numeroDos = input()
+numeroTres = input()
+
+if(numeroUno < 0):
+    producto = numeroUno * numeroDos * numeroTres
+    print (producto)
+else:
+    suma = numeroUno + numeroDos + numeroTres
+    print (suma)
